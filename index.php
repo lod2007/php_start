36ebb6e8d451df6c9d6320d1bdb0dde9e0c38397
lod2007 <lod2007@mail.ru>
2021-09-15 12:04:03 +0500

    Подправили цвета

--- index.php
+++ index.php
@@ -6,5 +6,7 @@ echo "Привет, Мир!";
 <a  href="page_2.php">Ссылка на вторую страницу</a>
 <br>
 <a  href="page_3.php">Ссылка на третью страницу</a>
+<br>
+<a  href="index.html">Ссылка на index.html</a>
 
 <? require_once 'footer.php'; ?>
